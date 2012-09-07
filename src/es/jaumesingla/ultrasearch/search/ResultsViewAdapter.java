8bc4b1e8574a8725d5bb62d20a13b417e82a463b
Jaume Singla <jaume.singlavalls@gmail.com>
2012-09-07 15:06:10 +0200

    Spinner results-- Discarted

--- src/es/jaumesingla/ultrasearch/search/ResultsViewAdapter.java
+++ src/es/jaumesingla/ultrasearch/search/ResultsViewAdapter.java
@@ -11,6 +11,7 @@ import android.content.Intent;
 import android.content.pm.ResolveInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.database.DataSetObserver;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.provider.Settings;
@@ -19,9 +20,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.CheckedTextView;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 
 public class ResultsViewAdapter extends BaseAdapter {
@@ -29,7 +36,8 @@ public class ResultsViewAdapter extends BaseAdapter {
 	private static class ViewHolder {
 		public TextView textView;
 		public ImageView imageView;
-		public Button infoView;
+		//public Button infoView;
+		public Spinner moreView;
 	}
 
 	protected static final String TAG = "ResultsViewAdapter";
@@ -38,6 +46,8 @@ public class ResultsViewAdapter extends BaseAdapter {
 	private LayoutInflater mInflater;
 	private Context mContext;
 	private int layoutId;
+
+	private SpinnerAdapter spinerAdapter;
 	
 	public ResultsViewAdapter(LayoutInflater inf, Context c, int layoutId){
 		super();
@@ -45,6 +55,42 @@ public class ResultsViewAdapter extends BaseAdapter {
 		listContents=new ArrayList<InfoLaunchApplication>();
 		mContext=c;
 		this.layoutId=layoutId;
+		
+		spinerAdapter=new BaseAdapter() {
+			private String[] data=mContext.getResources().getStringArray(R.array.app_options);
+			@Override
+			public View getView(int position, View convertView, ViewGroup parent) {
+				//Log.d(TAG, "getView");
+				ImageView v=new ImageView(mContext);
+				v.setImageResource(android.R.drawable.ic_menu_info_details);
+				return v;
+			}
+			
+			@Override
+			public long getItemId(int position) {
+				return position;
+			}
+			
+			@Override
+			public String getItem(int position) {
+				return data[position];
+			}
+			
+			@Override
+			public int getCount() {
+				return data.length;
+			}
+			
+			@Override
+			public View getDropDownView(int position, View convertView,ViewGroup parent) {
+				if (convertView==null){
+					convertView=mInflater.inflate(android.R.layout.simple_spinner_dropdown_item, null);
+				}
+				((CheckedTextView) convertView).setText(data[position]);
+				return convertView;
+				//return super.getDropDownView(position, convertView, parent);
+			}
+		};//*/ ArrayAdapter.createFromResource(mContext,  R.array.app_options, android.R.layout.simple_spinner_item );
 	}
 	
 	public void add(InfoLaunchApplication e){
@@ -84,12 +130,16 @@ public class ResultsViewAdapter extends BaseAdapter {
 			holder = new ViewHolder();
 			holder.textView = (TextView)convertView.findViewById(R.id.txtName);
 			holder.imageView = (ImageView)convertView.findViewById(R.id.imgIcon);
-			holder.infoView = (Button) convertView.findViewById(R.id.btInfo);
+			holder.moreView = (Spinner) convertView.findViewById(R.id.spOptions);
+			//holder.infoView = (Button) convertView.findViewById(R.id.btInfo);
 			//holder.labelPos = (TextView)convertView.findViewById(R.id.labelText);
-			assert(holder.textView!=null);
 			convertView.setTag(holder);
-			holder.infoView.setFocusable(false);
-			holder.infoView.setFocusableInTouchMode(false);
+			//holder.infoView.setFocusable(false);
+			//holder.infoView.setFocusableInTouchMode(false);
+			
+			holder.moreView.setAdapter(spinerAdapter);
+			holder.moreView.setFocusable(false);
+			holder.moreView.setFocusableInTouchMode(false);
 		} else {
 			holder = (ViewHolder)convertView.getTag();
 		}
@@ -105,13 +155,36 @@ public class ResultsViewAdapter extends BaseAdapter {
 		}
 		holder.textView.setText(toShow.getName());
 		holder.imageView.setImageDrawable(icon);
-		holder.infoView.setOnClickListener(new OnClickListener() {
+		
+		//ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(mContext,  R.array.app_options, android.R.layout.simple_spinner_item );
+		// Specify the layout to use when the list of choices appears
+		//adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);//*/
+		
+		
+		
+		holder.moreView.setOnItemSelectedListener(new OnItemSelectedListener() {
+			
+			int element=position;
+			@Override
+			public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+				Log.d("SpinnerItemSelected", "click:"+arg0+","+arg1+","+arg2+","+arg3);
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		
+		/*holder.infoView.setOnClickListener(new OnClickListener() {
 			int e=position;
 			@Override
 			public void onClick(View v) {
 				ResultsViewAdapter.this.viewInfo(e);
 			}
-		});
+		});*/
 		
 		/*convertView.setOnClickListener(new OnClickListener() {
 			int e=position;
